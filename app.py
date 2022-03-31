5f6f10336bd5dafeb322ab38bbca72d5dd3d1451
Georgia Skrempou <georgiaskrempou@gmail.com>
2022-03-31 22:42:48 +0200

    feat: initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from picamera import PiCamera
+#from time import sleep
+from boto3 import session
+from botocore.client import Config
+import os
+from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv() # a function that reads the .env file and stores those variables in os.env and makes them environment variables
+ACCESS_ID = os.environ["ACCESS_ID"]
+SECRET_KEY = os.environ["SECRET_KEY"]
+SPACES_URL = os.environ["SPACES_URL"]
+
+
+# initiate session with spaces
+session = session.Session()
+client = session.client("s3", 
+                        region_name = "ams3", 
+                        endpoint_url = SPACES_URL,
+                        aws_access_key_id = ACCESS_ID,
+                        aws_secret_access_key=SECRET_KEY)
+
+# capture image
+camera = PiCamera()
+now = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+camera.capture(f'/home/pi/camera-app/image{now}.jpeg')
+
+# upload image
+try:
+    client.upload_file(f"image{now}.jpeg", "picamera_photos", f"image{now}.jpeg")
+except Exception:
+    print("Failed client connection")
+    exit(1)
+
+# delete image
+os.remove(f"image{now}.jpeg")
+
