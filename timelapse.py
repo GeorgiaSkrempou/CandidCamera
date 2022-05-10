79c733b79d6d6e10bbf98b7ac1ac8959d9f17974
georgia <georgiaskrempou@gmail.com>
2022-05-10 16:16:40 +0200

    feat: added script for creating a timelapse video of the downloaded images

---
+++ timelapse.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+import glob
+
+# make an empty list
+img_array = []
+
+# loop through all files by filename in a sorted fashion
+for filename in sorted(glob.glob('picamera_photos/*.jpeg')):
+    # cerate an object with the images 
+    img = cv2.imread(filename)
+    # set the property shape of the img object
+    height, width, layers = img.shape
+    size = (width, height)
+    # append the img to the img_array list
+    img_array.append(img)
+
+# construct the video
+out = cv2.VideoWriter('timelapse.avi', cv2.VideoWriter_fourcc(*'DIVX'), 7, size)
+
+# write images in the video
+for i in range(len(img_array)):
+    out.write(img_array[i])
+
+# release video
+out.release()
